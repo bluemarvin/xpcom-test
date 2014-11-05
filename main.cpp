16c7074bfb390e97ebe202ad517335426dc812ec
Randall Barker <rbarker@mozilla.com>
2014-11-04 16:00:29 -0800

    Updated to test timers.

--- main.cpp
+++ main.cpp
@@ -4,43 +4,68 @@
 #include "nsISupports.h"
 #include "nsRefPtr.h"
 #include "nsCOMPtr.h"
+#include "nsComponentManagerUtils.h"
 #include "nsAutoPtr.h"
 #include "mozilla/Mutex.h"
 #include "iTest.h"
 #include "nsIRunnable.h"
+#include "nsIThread.h"
+#include "nsITimer.h"
 #include "nsThreadUtils.h"
 
 #include <stdio.h>
 
-class nsRunner : public nsIRunnable {
+class Runner : public nsIRunnable {
 public:
-    NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_THREADSAFE_ISUPPORTS
 
-    NS_IMETHOD Run() {
-        nsCOMPtr<nsIThread> thread;
-        nsresult rv = NS_GetCurrentThread(getter_AddRefs(thread));
-        if (NS_FAILED(rv)) {
-            printf("failed to get current thread\n");
-            return rv;
-        }
-        printf("running %d on thread %p\n", mNum, (void *)thread.get());
+  NS_IMETHOD Run() {
+      nsCOMPtr<nsIThread> thread;
+      nsresult rv = NS_GetCurrentThread(getter_AddRefs(thread));
+      if (NS_FAILED(rv)) {
+          fprintf(stderr, "failed to get current thread\n");
+          return rv;
+      }
+      fprintf(stderr, "running %d on thread %p\n", mNum, (void *)thread.get());
+      return rv;
+  }
 
-        // if we don't do something slow, we'll never see the other
-        // worker threads run
-        PR_Sleep(PR_MillisecondsToInterval(100));
-
-        return rv;
-    }
-
-    nsRunner(int num) : mNum(num) {
-    }
+  Runner(int num) : mNum(num) {}
 
 protected:
-    virtual ~nsRunner() {}
-    int mNum;
+  virtual ~Runner() {}
+  int mNum;
 };
 
-NS_IMPL_ISUPPORTS(nsRunner, nsIRunnable)
+NS_IMPL_ISUPPORTS(Runner, nsIRunnable)
+
+class TimerCallback MOZ_FINAL : public nsITimerCallback
+{
+public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+
+  TimerCallback(nsCOMPtr<nsIThread>& aTarget, bool& aQuit) : mTarget(aTarget), mQuit(aQuit), mCount(0) {}
+
+  NS_IMETHOD Notify(nsITimer* aTimer) {
+    fprintf(stderr, "Got timer callback\n");
+    nsRefPtr<Runner> runner = new Runner(mCount);
+    mCount++;
+    mTarget->Dispatch(runner, NS_DISPATCH_NORMAL);
+
+    if (mCount >= 5) {
+      mQuit = true;
+    }
+
+    return NS_OK;
+  }
+private:
+  ~TimerCallback() {}
+  nsCOMPtr<nsIThread> mTarget;
+  bool& mQuit;
+  int mCount;
+};
+
+NS_IMPL_ISUPPORTS(TimerCallback, nsITimerCallback)
 
 class Test : public iTest
 {
@@ -89,12 +114,21 @@ main(int argc, char* argv[])
 
   nsresult rv;
 
-  nsCOMPtr<nsIRunnable> event = new nsRunner(0);
-  if (!event)
-    return 0;
+  nsCOMPtr<nsIThread> thread;
+  rv = NS_NewThread(getter_AddRefs(thread));
 
-  nsCOMPtr<nsIThread> runner;
-  rv = NS_NewThread(getter_AddRefs(runner), event);
+  bool quit = false;
+
+  nsCOMPtr<nsITimer> timer = do_CreateInstance(NS_TIMER_CONTRACTID, &rv);
+
+  nsCOMPtr<nsITimerCallback> callback = new TimerCallback(thread, quit);
+
+  rv = timer->InitWithCallback(callback, PR_MillisecondsToInterval(1000),
+                               nsITimer::TYPE_REPEATING_PRECISE);
+
+  while(!quit) {
+    NS_ProcessNextEvent();
+  }
 
   NS_ShutdownXPCOMRT();
   return 0;
